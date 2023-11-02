c534bd9d94192de729204fc664d93527118dd0c9
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-02 17:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function fibonacci(num) {
 // your code here
-}
-
-module.exports = fibonacci;
+	if(num > 0)
+	{
+		let a=0; 
+	    let	 b=1;
+		for(let i=2; i<=num; i++)
+		{
+			let temp = a;
+			a = b;
+			b = temp + a;
+			
+		}
+	
+		return a;
+	}	
+		else{
+		return "Enter a positive number";
+		}
+	}
+  
+let num = parseFloat(prompt("Enter a Positive Number"));
+alert(fibonacci(num));
+    
+          
